19d646c46b944a7d3d93f1a8a0e49dcc96b86e06
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2023-11-21 12:00:16 +0300

    Add files via upload

---
+++ hastane v1-826/Hasta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hastane_v1_826
+{
+    internal class Hasta
+    {
+    }
+}
